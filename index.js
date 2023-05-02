4f0ab6df10caafaeb057a7b5fcc5249e3889d1fd
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-02 16:54:49 +0600

    update json

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
+const allData = require("./data.json");
 
 app.get("/", (req, res) => {
   res.json({ message: "hello server running" });
 });
-
+app.get("/allData", (req, res) => {
+  res.json(allData);
+});
 app.listen(5000, () => {
   console.log("server is running");
 });
