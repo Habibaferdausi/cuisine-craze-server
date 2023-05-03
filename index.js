208bc19a17b1363ce2cfbbd4340926bb892c20c7
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-03 13:07:37 +0600

    git ignore update

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ const allData = require("./data.json");
 app.get("/", (req, res) => {
   res.json({ message: "hello server running" });
 });
+
 app.get("/allData", (req, res) => {
   res.json(allData);
 });
+
+app.get("/allData/:id", (req, res) => {
+  const id = req.params.id;
+  const item = allData.find((pd) => pd.id === parseInt(id));
+  res.send({ item });
+});
+
 app.listen(5000, () => {
   console.log("server is running");
 });
